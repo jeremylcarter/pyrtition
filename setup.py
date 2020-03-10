705cdfc751fe08a86384361b73823dcd0b6c2e3b
Jeremy Carter <jeremy.carter@ampcontrolgroup.com>
2020-03-10 14:45:03 +1100

    Added missing module files.

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ from setuptools import setup, find_packages
 setup(
     name="pyrtition",
     version="1.0",
-    packages=find_packages(),
+    packages=find_packages(exclude="tests"),
     license='MIT',
     author='Jeremy Carter',
     author_email='jeremychild@gmail.com',
+    python_requires='>=3.6',
 )
