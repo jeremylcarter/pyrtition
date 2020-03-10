402e776a4fdc34e56888530659be06c2b6e2e07a
Jeremy Carter <jeremy.carter@ampcontrolgroup.com>
2020-03-10 14:54:15 +1100

    Updated to sem versioning

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyrtition",
-    version="1.0",
+    version="1.0.1",
     packages=find_packages(exclude="tests"),
     license='MIT',
     author='Jeremy Carter',
