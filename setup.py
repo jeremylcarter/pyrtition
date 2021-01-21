7d1f40995a09bb71154ad040e33058e7d8f7de0e
Jeremy Carter <jeremy.carter@ampcontrolgroup.com>
2021-01-21 12:46:57 +1100

    Updated version to 1.0.5.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyrtition",
-    version="1.0.4",
+    version="1.0.5",
     packages=find_packages(exclude="tests"),
     license='MIT',
     author='Jeremy Carter',
